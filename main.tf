1eb57c7ae5a36e96c885f4819f13dc83604f9a30
sbarrett847 <sbarrett847@gmail.com>
2021-03-26 10:54:39 +0000

    Restart git commits with gitignore

--- main.tf
+++ main.tf
@@ -16,22 +16,22 @@ provider "azurerm" {
 
 
 # Create a resource group
-resource "azurerm_resource_group" "rgtest1" {
-  name     = "TerraForm-Test1-RG"
+resource "azurerm_resource_group" "SB-rgtest1" {
+  name     = "SB-TerraForm-Test1-RG"
   location = "North Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "vnettest1" {
-  name                = "TerraForm-Test1-vNet"
+resource "azurerm_virtual_network" "SB-vnettest1" {
+  name                = "SB-TerraForm-Test1-vNet"
   resource_group_name = azurerm_resource_group.rgtest1.name
   location            = azurerm_resource_group.rgtest1.location
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnettest1" {
-  name                 = "Terraform-Test1-subnet"
+resource "azurerm_subnet" "SB-subnettest1" {
+  name                 = "SB-Terraform-Test1-subnet"
   resource_group_name  = azurerm_resource_group.rgtest1.name
   virtual_network_name = azurerm_virtual_network.vnettest1.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
